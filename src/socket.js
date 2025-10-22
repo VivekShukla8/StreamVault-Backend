6ead53abb54d96f63da5621138b17eee3483926e
VivekShukla8 <vivekshukla8823@gmail.com>
2025-10-22 12:06:11 +0530

    Socket connection issues

--- src/socket.js
+++ src/socket.js
@@ -10,7 +10,7 @@ export const initSocket = (httpServer) => {
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        process.env.FRONTEND_URL
+        "https://stream-vault-frontend1.vercel.app/#/"
       ].filter(Boolean),
       credentials: true,
       methods: ["GET", "POST"]
